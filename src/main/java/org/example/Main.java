b5b12219cab23dea86c57ad337207921973580a3
Andrii Bozhenko <a.s.bozhenko@gmail.com>
2024-09-03 10:51:38 +0200

    Reg-03: Add new pet

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import org.example.component.ApplicationRunner;
 
 import java.util.Scanner;
-1
+
 public class Main {
 
     public static Scanner SCANNER = new Scanner(System.in);
